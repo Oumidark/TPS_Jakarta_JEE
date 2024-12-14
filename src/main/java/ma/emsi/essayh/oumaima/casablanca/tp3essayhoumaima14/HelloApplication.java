a2cec91de98a77443b401e7edc3313496da7f491
Oumaima Essayh <129409129+Oumidark@users.noreply.github.com>
2024-12-14 16:01:12 +0100

    Création du projet tp3

---
+++ src/main/java/ma/emsi/essayh/oumaima/casablanca/tp3essayhoumaima14/HelloApplication.java
@@ -0,0 +1,9 @@
+package ma.emsi.essayh.oumaima.casablanca.tp3essayhoumaima14;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
