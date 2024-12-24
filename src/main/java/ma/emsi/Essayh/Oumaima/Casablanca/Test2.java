68f87e3f9527b1ba1621f6385838b8d2308869af
Oumaima Essayh <129409129+Oumidark@users.noreply.github.com>
2024-12-24 17:35:35 +0100

    Test2 done

--- src/main/java/ma/emsi/Essayh/Oumaima/Casablanca/Test2.java
+++ src/main/java/ma/emsi/Essayh/Oumaima/Casablanca/Test2.java
@@ -1,8 +1,112 @@
 package ma.emsi.Essayh.Oumaima.Casablanca;
 
+import dev.langchain4j.data.document.Document;
+import dev.langchain4j.data.document.DocumentParser;
+import dev.langchain4j.data.document.DocumentSplitter;
+import dev.langchain4j.data.document.loader.FileSystemDocumentLoader;
+import dev.langchain4j.data.document.parser.apache.tika.ApacheTikaDocumentParser;
+import dev.langchain4j.data.document.splitter.DocumentSplitters;
+import dev.langchain4j.memory.ChatMemory;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import dev.langchain4j.model.chat.ChatLanguageModel;
+import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.model.embedding.onnx.allminilml6v2.AllMiniLmL6V2EmbeddingModel;
+import dev.langchain4j.model.googleai.GoogleAiGeminiChatModel;
+import dev.langchain4j.rag.DefaultRetrievalAugmentor;
+import dev.langchain4j.rag.RetrievalAugmentor;
+import dev.langchain4j.rag.content.retriever.ContentRetriever;
+import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
+import dev.langchain4j.rag.query.transformer.CompressingQueryTransformer;
+import dev.langchain4j.rag.query.transformer.QueryTransformer;
+import dev.langchain4j.service.AiServices;
+import dev.langchain4j.store.embedding.EmbeddingStore;
+import dev.langchain4j.store.embedding.EmbeddingStoreIngestor;
+import dev.langchain4j.store.embedding.inmemory.InMemoryEmbeddingStore;
+
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 public class Test2 {
     public static void main(String[] args) {
+        ChatLanguageModel model = GoogleAiGeminiChatModel.builder()
+                .temperature(0.7)
+                .modelName("gemini-1.5-flash")
+                .apiKey(System.getenv("GEMINI_KEY"))
+                .build();
+
+        Path pathRessource;
+        try {
+            String cheminRessource = "/Cours-Machine-learning.pdf"; // chemin absolu
+            // MaClass désigne, par exemple, la classe qui contient ce code.
+            URL fileUrl = RagNaif.class.getResource(cheminRessource);
+            pathRessource = Paths.get(fileUrl.toURI());
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e); // ou un autre traitement du problème...
+        }
+
+        DocumentParser parser = new ApacheTikaDocumentParser();
+
+        Document document = FileSystemDocumentLoader.loadDocument(pathRessource, parser);
+
+        DocumentSplitter documentSplitter = DocumentSplitters.recursive(200, 10);
+
+        //List<TextSegment> chunks = documentSplitter.split(document);
+
+        EmbeddingModel embeddingModel = new AllMiniLmL6V2EmbeddingModel();
+
+        //Response<List<Embedding>> embeddings = embeddingModel.embedAll(chunks);
+
+        EmbeddingStore embeddingStore = new InMemoryEmbeddingStore();
+
+        EmbeddingStoreIngestor ingestor = EmbeddingStoreIngestor.builder()
+                .embeddingStore(embeddingStore)
+                .embeddingModel(embeddingModel)
+                .documentSplitter(documentSplitter)
+                .build();
+
+        ingestor.ingest(document);
 
 
+        ContentRetriever contentRetriever = EmbeddingStoreContentRetriever.builder()
+                .embeddingModel(embeddingModel)
+                .embeddingStore(embeddingStore)
+                .minScore(0.5)
+                .maxResults(2)
+                .build();
+
+        QueryTransformer queryTransformer = CompressingQueryTransformer.builder()
+                .chatLanguageModel(model)
+                .build();
+
+        RetrievalAugmentor retrievalAugmentor = DefaultRetrievalAugmentor.builder()
+                .contentRetriever(contentRetriever)
+                .queryTransformer(queryTransformer)
+                .build();
+
+        ChatMemory chatMemory = MessageWindowChatMemory.withMaxMessages(10); //n'est pas utilisé
+
+        Assistant assistant = AiServices.builder(Assistant.class)
+                .chatLanguageModel(model)
+                .chatMemory(chatMemory)
+                .retrievalAugmentor(retrievalAugmentor)
+                .build();
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+                System.out.println("==================================================");
+                System.out.println("Posez votre question : ");
+                String question = scanner.nextLine();
+                System.out.println("==================================================");
+                if ("fin".equalsIgnoreCase(question)) {
+                    break;
+                }
+                String reponse = assistant.chat(question);
+                System.out.println("==================================================");
+                System.out.println("Assistant : " + reponse);
+            }
+        }
     }
 }
